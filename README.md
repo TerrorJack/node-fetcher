2e2a84afeb6a2202e50a847d1d9c9dc387e774ae
Shao Cheng <cheng.shao@tweag.io>
2019-11-03 19:59:54 +0800

    Support fetching v8 node builds

--- README.md
+++ README.md
@@ -1,23 +1,37 @@
 # `node-fetcher`
 
-A script to fetch and extract `node` release tarballs. Supports `win-x64`,
-`linux-x64` and `darwin-x64`. Useful for testing multiple versions of `node` on
-CI.
+A script to fetch and extract `node` release tarballs, useful for testing
+multiple versions of `node` on CI.
 
 ## Usage
 
 ```
-usage: node-fetcher.py [-h] [--channel CHANNEL] [--version VERSION]
-                       [--path PATH]
+usage: node-fetcher.py [-h] [--channel CHANNEL] [--platform PLATFORM]
+                       [--version VERSION] [--path PATH]
 
 optional arguments:
-  -h, --help         show this help message and exit
-  --channel CHANNEL  The release channel to check, e.g. v8-canary, defaults to
-                     release
-  --version VERSION  The version string prefix to match, e.g. v13
-  --path PATH        The destination to extract, defaults to current working
-                     directory
+  -h, --help           show this help message and exit
+  --channel CHANNEL    The release channel to check, e.g. "v8-canary",
+                       defaults to "release"
+  --platform PLATFORM  Supported platforms: {win,linux,darwin}-x64, defaults
+                       to current platform
+  --version VERSION    The version string prefix to match, e.g. v13
+  --path PATH          The destination to extract, defaults to current working
+                       directory
+
 ```
+
+Check the `node` [download](https://nodejs.org/download/) page for a list of
+channels.
+
+Additionally, we support a `v8` channel which fetches the V8 team's latest
+successful
+[build](https://ci.chromium.org/p/v8/builders/ci/V8%20Linux64%20-%20node.js%20integration%20ng).
+The `--channel v8` flag will ignore `--platform` and `--version`, since the V8
+team only provides linux x64 builds, and there isn't a meaningful version number
+for those builds. Also, keep in mind that compared to the `v8-canary` builds
+provided by the `node` folks, the `v8` builds only comes with a `bin/node`
+executable without `npm` packaged.
 
 ## Why not `nvm`?
 
--- node-fetcher.py
+++ node-fetcher.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from argparse import ArgumentParser
+from html.parser import HTMLParser
 from io import BytesIO
 import json
 import os
@@ -14,6 +15,10 @@ from zipfile import ZipFile
 
 def get_bytes(url):
     return urlopen(url).read()
+
+
+def get_str(url):
+    return get_bytes(url).decode()
 
 
 def get_node_index(chan):
@@ -62,20 +67,70 @@ def extract_node_zip(chan, platform, ver, path):
     sub_path.rmdir()
 
 
+class V8SuccessBuildURLParser(HTMLParser):
+    def __init__(self):
+        super().__init__()
+        self.flag = False
+        self.url = None
+
+    def handle_starttag(self, tag, attrs):
+        if self.url:
+            return
+        if attrs == [("class", "status SUCCESS")]:
+            self.flag = True
+        if self.flag and tag == "a":
+            self.url = "https://ci.chromium.org{0}".format(attrs[0][1])
+
+
+def get_node_v8_success_build_url():
+    p = V8SuccessBuildURLParser()
+    p.feed(get_str("https://ci.chromium.org/p/v8/builders/luci.v8.ci/V8%20Linux64%20-%20node.js%20integration%20ng"))
+    return p.url
+
+
+class V8ZipURLParser(HTMLParser):
+    def __init__(self):
+        super().__init__()
+        self.url = None
+
+    def handle_starttag(self, tag, attrs):
+        if len(attrs) >= 3 and attrs[2][1] and attrs[2][1].startswith("https://storage.googleapis.com/chromium-v8/node-linux-rel"):
+            self.url = attrs[2][1]
+
+
+def get_node_v8_zip_url(build_url):
+    p = V8ZipURLParser()
+    p.feed(get_str(build_url))
+    return p.url
+
+
+def extract_node_v8_zip(path):
+    with ZipFile(BytesIO(get_bytes(get_node_v8_zip_url(get_node_v8_success_build_url())))) as z:
+        z.extractall(path=path)
+    for f in Path(path, "bin").iterdir():
+        f.chmod(0o755)
+
+
 if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument(
-        "--channel", help="The release channel to check, e.g. v8-canary, defaults to release")
+        "--channel", help="The release channel to check, e.g. \"v8-canary\", defaults to \"release\"")
+    parser.add_argument(
+        "--platform", help="Supported platforms: {win,linux,darwin}-x64, defaults to current platform")
     parser.add_argument(
         "--version", help="The version string prefix to match, e.g. v13")
     parser.add_argument(
         "--path", help="The destination to extract, defaults to current working directory")
     args = parser.parse_args()
-    chan = args.channel if args.channel else "release"
-    platform = {"win32": "win-x64", "linux": "linux-x64",
-                "darwin": "darwin-x64"}[sys.platform]
-    ver = get_node_ver(get_node_index(chan), platform,
-                       args.version if args.version else "")
     path = args.path if args.path else os.getcwd()
-    (extract_node_zip if platform ==
-     "win-x64" else extract_node_tar)(chan, platform, ver, path)
+    chan = args.channel if args.channel else "release"
+    if chan == "v8":
+        extract_node_v8_zip(path)
+    else:
+        platform = args.platform if args.platform else {"win32": "win-x64", "linux": "linux-x64",
+                                                        "darwin": "darwin-x64"}[sys.platform]
+        ver = get_node_ver(get_node_index(chan), platform,
+                           args.version if args.version else "")
+
+        (extract_node_zip if platform ==
+         "win-x64" else extract_node_tar)(chan, platform, ver, path)
